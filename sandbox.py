75d4fca3d1493bd201cc2c03821c06e05c9eda13
Avaxar <xapaxaavatar@gmail.com>
2020-12-14 18:29:08 +0700

    Thanks, very cool power ranger wookie
    Discovered that with changing a function's __code__ property, it could execute nasty code.

--- sandbox.py
+++ sandbox.py
@@ -144,7 +144,7 @@ async def exec_sandbox(code: str, timeout=5, max_memory=2 ** 28):
 
     allowed_globals["output"] = output
 
-    for illegal_patterns in ["__subclasses__", "__loader__", "__bases__", "mro"]:
+    for illegal_patterns in ["__subclasses__", "__loader__", "__bases__", "__code__", "mro"]:
         if illegal_patterns in code:
             output.exc = Exception("Suspicious Pattern")
             return output
