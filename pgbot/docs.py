90664eb3d2ebb11f39c4ece4532b5bb2ca6b805f
Brandon <bmcomis2018@gmail.com>
2021-03-23 15:20:01 -0500

    ability to input builtin classes directly in pg\!doc

--- pgbot/docs.py
+++ pgbot/docs.py
@@ -78,16 +78,25 @@ def get(name):
 
     for part in splits:
         try:
-            obj = objects[part]
+            try:
+                is_builtin = getattr(builtins, part)
+            except AttributeError:
+                is_builtin = False
+
+            if is_builtin:
+                obj = is_builin
+            else:
+                obj = objects[part]
+
             try:
                 objects = vars(obj)
             except BaseException:  # TODO: Figure out proper exception
                 objects = {}
         except KeyError:
             return (
-                "Class/function/sub-module not found!", 
-                f"There's no such thing here named `{name}`"
-            )
+                    "Class/function/sub-module not found!",
+                    f"There's no such thing here named `{name}`"
+                )
 
     messg = str(obj.__doc__).replace("```", common.ESC_CODE_BLOCK_QUOTE)
 
