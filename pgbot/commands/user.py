a43e3a69fe3f8ee2431c78d9f081b8c96ca9590b
Just-JC <65417594+Just-JC@users.noreply.github.com>
2021-05-09 19:52:00 +0200

    fix bug in previous commit

--- pgbot/commands/user.py
+++ pgbot/commands/user.py
@@ -212,6 +212,8 @@ class UserCommand(BaseCommand):
         -----
         Implement pg!exec, for execution of python code
         """
+        print("EXEC TIME")
+        print(code.code)
         tstamp = time.perf_counter_ns()
         await self.channel.trigger_typing()
 
