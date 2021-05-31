d4d7aae778db490ff7fe3f80a998a65abd9f38b1
Ankith <46915066+ankith26@users.noreply.github.com>
2021-05-31 19:36:39 +0530

    minor bug fix in command handler, improve pg!ping

--- pgbot/commands/__init__.py
+++ pgbot/commands/__init__.py
@@ -86,23 +86,24 @@ async def handle(invoke_msg: discord.Message, response_msg: discord.Message = No
             )
         sys.exit(0)
 
-    if not common.TEST_MODE:
-        if not common.GENERIC:
-            await embed_utils.send_2(
-                common.log_channel,
-                title=f"Command invoked by {invoke_msg.author} / {invoke_msg.author.id}",
-                description=discord.utils.escape_markdown(invoke_msg.content),
-                fields=(
-                    ("\u200b", f"**[View Original]({invoke_msg.jump_url})**", False),
-                ),
-            )
-
-    elif common.TEST_USER_IDS and invoke_msg.author.id not in common.TEST_USER_IDS:
+    if (
+        common.TEST_MODE
+        and common.TEST_USER_IDS
+        and invoke_msg.author.id not in common.TEST_USER_IDS
+    ):
         return
 
     if response_msg is None:
         response_msg = await embed_utils.send(
             invoke_msg.channel, "Your command is being processed!", ""
+        )
+
+    if not common.TEST_MODE and not common.GENERIC:
+        await embed_utils.send_2(
+            common.log_channel,
+            title=f"Command invoked by {invoke_msg.author} / {invoke_msg.author.id}",
+            description=discord.utils.escape_markdown(invoke_msg.content),
+            fields=(("\u200b", f"**[View Original]({invoke_msg.jump_url})**", False),),
         )
 
     cmd = (
