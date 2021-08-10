e122e30376d0f6f0ea03cec16e616a6f0b1f4183
Just-JC <65417594+Just-JC@users.noreply.github.com>
2021-08-10 21:28:40 +0200

    Add __init__.py to /tasks

---
+++ pgbot/tasks/__init__.py
@@ -0,0 +1,9 @@
+"""
+This file is a part of the source code for the PygameCommunityBot.
+This project has been licensed under the MIT license.
+Copyright (c) 2020-present PygameCommunityDiscord
+
+This file exports the task class system. 
+"""
+
+from . import core, defaults, events
